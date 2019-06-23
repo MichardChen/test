ae4e69342ae6eeaaf896b5974f554f66880e1ebe
MichardChen <765205059@qq.com>
2019-06-23 13:46:41 +0800

    init

---
+++ src/main/java/com/etc/entity/User.java
@@ -0,0 +1,17 @@
+package com.etc.entity;
+
+public class User {
+	
+	private String name = "root";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
